99e77b5957753c2782ac990610a0da90ad23986c
IMakeBotsForYou <40301997+IMakeBotsForYou@users.noreply.github.com>
2020-04-06 15:29:24 +0300

    Create intro.asm

---
+++ src/intro.asm
@@ -0,0 +1,26 @@
+text   db "",10,13
+	db '                                   ',10,13
+	db 'I  N  S  T  R  U  C  T  I  O  N  S',10,13
+	db '                                   ',10,13
+	db '===================================',10,13
+	db '                                   ',10,13
+	db 'Background Color:    UP / DOWN',10,13
+	db 'Change Color:     RIGHT / LEFT',10,13
+	db 'Draw Circle At mouse:        C',10,13
+	db 'Set First Point              L',10,13
+	db 'Set Second Point/draw rect   T',10,13
+	db 'Clear Screen         Backspace',10,13
+	db 'Exit Program:              ESC',10,13
+	db '                              ',10,13
+	db '[Press any key to start drawing!]',10,13
+	db '                                   ',10,13
+	db '                                   ',10,13
+	db '                                   ',10,13
+	db '                                   ',10,13
+	db '______           ______     _       _   ',10,13
+	db '|  _  \          | ___ \   (_)     | |  ',10,13
+	db '| | | |__ _ _ __ | |_/ /_ _ _ _ __ | |_ ',10,13
+	db '| | | / _` |  _ \|  __/ _` | |  _ \| __|',10,13
+	db '| |/ / (_| | | | | | | (_| | | | | | |_ ',10,13
+	db '|___/ \__,_|_| |_\_|  \__,_|_|_| |_|\__|',10,13
+	db 'by Dan Lvov, ASCII ART http://www.patorjk.com/$'
