bfd484b270ea8926dd311228779dedc6a325e76d
IMakeBotsForYou <40301997+IMakeBotsForYou@users.noreply.github.com>
2020-04-15 15:43:22 +0300

    Update intro.asm

--- src/intro.asm
+++ src/intro.asm
@@ -8,7 +8,16 @@ text   db "",10,13
 	db 'Change Color:     RIGHT / LEFT',10,13
 	db 'Draw Circle At mouse:        C',10,13
 	db 'Set Corners/Draw Rectangle   L',10,13
+	db 'Set centre of square         S',10,13
 	db 'Set Points/draw diagonal     T',10,13
+	db '                              ',10,13
+	db 'Inc/Dec size of square     ] [',10,13
+	db 'Inc/Dec size of circle     + -',10,13
+	db '                              ',10,13
+	db 'Set second point/draw rect   ;',10,13
+	db 'Set second point/draw diag.  0',10,13
+	db '                              ',10,13
+	db 'See this screen again        H',10,13
 	db 'Clear Screen         Backspace',10,13
 	db 'Exit Program:              ESC',10,13
 	db '                              ',10,13
