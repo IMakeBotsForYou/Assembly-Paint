62fb36d65c047cbd89f1669d94ee2a3d23d70b73
IMakeBotsForYou <40301997+IMakeBotsForYou@users.noreply.github.com>
2020-04-15 15:45:04 +0300

    made fit

--- src/intro.asm
+++ src/intro.asm
@@ -10,20 +10,15 @@ text   db "",10,13
 	db 'Set Corners/Draw Rectangle   L',10,13
 	db 'Set centre of square         S',10,13
 	db 'Set Points/draw diagonal     T',10,13
-	db '                              ',10,13
 	db 'Inc/Dec size of square     ] [',10,13
 	db 'Inc/Dec size of circle     + -',10,13
-	db '                              ',10,13
 	db 'Set second point/draw rect   ;',10,13
 	db 'Set second point/draw diag.  0',10,13
-	db '                              ',10,13
 	db 'See this screen again        H',10,13
 	db 'Clear Screen         Backspace',10,13
 	db 'Exit Program:              ESC',10,13
 	db '                              ',10,13
 	db '[Press any key to start drawing!]',10,13
-	db '                                   ',10,13
-	db '                                   ',10,13
 	db '                                   ',10,13
 	db '                                   ',10,13
 	db '______           ______     _       _   ',10,13
